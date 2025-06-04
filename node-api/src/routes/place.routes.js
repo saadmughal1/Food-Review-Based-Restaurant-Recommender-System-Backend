13ccf095c6a1d4b8857b5662684bcdda0f1e3873
saad mughal <saadmughalxms@gmail.com>
2025-06-04 17:15:44 +0500

    feat: add photo proxy route to fetch images from Google Maps API

--- node-api/src/routes/place.routes.js
+++ node-api/src/routes/place.routes.js
@@ -6,10 +6,12 @@ import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 import {
   place,
-  searchPlaces
+  searchPlaces,
+  photoProxy
 } from "../controllers/place.controller.js";
 
 router.route("/").get(asyncHandler(searchPlaces));
+router.route("/photo-proxy").get(asyncHandler(photoProxy));
 router.route("/:placeid").get(asyncHandler(place));
 
 export default router;
